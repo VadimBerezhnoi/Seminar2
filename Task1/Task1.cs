bf7f69724603c0f155d7490ce40f48bdc89a7cb0
VadimBerezhnoi <tsigma66@gmail.com>
2023-02-02 20:44:44 +0300

    Написали программу по определению положительной или отрицательной координаты в четвертях координатной плоскости

---
+++ Task1/Task1.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+
+Console.Write("Введите номер четверти координатной плоскости: ");
+int quarter = int.Parse(Console.ReadLine()!);
+
+while (quarter < 1 || quarter > 4)
+{
+    Console.Write("Введите номер четверти координатной плоскости: ");
+    quarter = int.Parse(Console.ReadLine()!);
+}
+
+if (quarter == 1)
+{
+    Console.WriteLine("X > 0 и Y > 0");
+}
+
+else if (quarter == 2)
+{
+    Console.WriteLine("X < 0 и Y > 0");
+}
+
+else if (quarter  == 3)
+{
+    Console.WriteLine("X < 0 и Y < 0");
+}
+
+else if (quarter == 4)
+{
+    Console.WriteLine("X > 0 и Y < 0");
+}
+
